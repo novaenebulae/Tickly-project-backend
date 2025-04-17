6e7ff3a3ac0ca3c91961e5055ad2a144c9b44f78
novenebulae <mail.lucas.olivarez@gmail.com>
2025-04-17 11:07:41 +0200

    Added env config with .env

--- src/main/java/edu/cda/project/ticklybackend/security/jwt/JwtUtils.java
+++ src/main/java/edu/cda/project/ticklybackend/security/jwt/JwtUtils.java
@@ -3,27 +3,32 @@ package edu.cda.project.ticklybackend.security.jwt;
 import edu.cda.project.ticklybackend.security.user.AppUserDetails;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.Map;
 
+
 @Service
 public class JwtUtils {
+
+    @Value("${jwt.secret")
+    String jwtSecret;
 
     public String generateJwtToken(AppUserDetails userDetails) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .addClaims(Map.of("role", userDetails.getRole()))
                 .setIssuedAt(new Date())
-                .signWith(SignatureAlgorithm.HS256, "azerty")
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
                 .compact();
     }
 
     public String getSubjectFromJwt(String jwt) {
 
         return Jwts.parser()
-                .setSigningKey("azerty")
+                .setSigningKey(jwtSecret)
                 .parseClaimsJws(jwt)
                 .getBody()
                 .getSubject();
